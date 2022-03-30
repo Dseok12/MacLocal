47fd7636e5c14b41ddca8b2b514dfa6179cc37a7
Dseok <ehskstkrjsa@naver.com>
2022-03-30 16:12:36 +0900

    React: EventPractice

--- React_File/Study/Study01/s1/src/App.js
+++ React_File/Study/Study01/s1/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 // import MyComponent from "./MyComponent";
 // import Counter from "./Counter";
-import Say from "./Say";
+// import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 function App() {
   return (
     <div>
       {/* <MyComponent>App.js에서 뿌려주는 값</MyComponent> */}
       {/* <Counter /> */}
-      <Say/>
+      {/* <Say/> */}
+      <EventPractice />
     </div>
   );
 }
---
+++ React_File/Study/Study01/s1/src/EventPractice.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  render(){
+    return(
+      <div>
+        <h1>
+          이벤트 연습
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default EventPractice;
