b81279bdbed8f43fdd12d92e0e7702d6a0aca2a3
D.seok <ehskstkrjsa@naver.com>
2022-04-01 18:25:33 +0900

    React: ValidationSample

--- React_File/Study/Study01/s1/src/App.js
+++ React_File/Study/Study01/s1/src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {Component} from "react";
 // import MyComponent from "./MyComponent";
 // import Counter from "./Counter";
 // import Say from "./Say";
 // import EventPractice from "./EventPractice";
-import EventPractice2 from "./EventPractice2";
+// import EventPractice2 from "./EventPractice2";
+import ValidationSample from "./ValidationSample";
 
-function App() {
-  return (
-    <div>
-      {/* <MyComponent>App.js에서 뿌려주는 값</MyComponent> */}
-      {/* <Counter /> */}
-      {/* <Say/> */}
-      {/* <EventPractice /> */}
-      <EventPractice2 />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {/* <MyComponent>App.js에서 뿌려주는 값</MyComponent> */}
+        {/* <Counter /> */}
+        {/* <Say/> */}
+        {/* <EventPractice /> */}
+        {/* <EventPractice2 /> */}
+        <ValidationSample />
+      </div>
+    );
+  }
 }
 
 export default App;
