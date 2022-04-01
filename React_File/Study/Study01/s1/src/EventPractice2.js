2a1a843e6be880760b2f2365e3bb9556b33e0b97
D.seok <ehskstkrjsa@naver.com>
2022-04-01 12:18:45 +0900

    React: EventPractice2

--- React_File/Study/Study01/s1/src/EventPractice2.js
+++ React_File/Study/Study01/s1/src/EventPractice2.js
@@ -1,7 +1,44 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 
 const EventPractice2 = () => {
-  
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = e => setUsername(e.target.value);
+  const onChangeMessage = e => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ': ' + message)
+    setUsername('');
+    setMessage('');
+  }
+
+  const onKeyPress = e => {
+    if(e.key === 'Enter'){
+      onClick();
+    }
+  }
+
+  return(
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder='사용자명'
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder='아무거나 입력해 보세요'
+        value={message}
+        onChange={onChangeMessage}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  )
+
 }
 
 export default EventPractice2;
