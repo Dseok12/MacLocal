1b4f64af2d89fc553013963b37b7218dbc36ed0a
D.seok <ehskstkrjsa@naver.com>
2022-04-03 00:10:40 +0900

    React: TodoApp setting

---
+++ React_File/Study/Study01/todoapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoTemplate from "./TodoTemplate/TodoTemplate";
+
+const App = () => {
+  return (
+    <div>
+      <TodoTemplate>TodoApp을 만들자</TodoTemplate>
+    </div>
+  );
+}
+
+export default App;
