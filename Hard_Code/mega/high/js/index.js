1df7d8d3af40421b9e17569e79e8218f49a39321
D.seok <ehskstkrjsa@naver.com>
2022-04-03 23:21:48 +0900

    Mega: Side Popup Toggle

--- Hard_Code/mega/high/js/index.js
+++ Hard_Code/mega/high/js/index.js
@@ -1,4 +1,20 @@
+$(document).ready(function(){
 
+
+  $('.side_utill_toggle_btn').on('click', function(){
+    $('.utill_list').toggle();
+
+    if($('.utill_list').css('display') == 'none'){
+      $('.side_utill_toggle_btn span').text('열기')
+    } else {
+      $('.side_utill_toggle_btn span').text('접기')
+    };
+
+  })
+
+
+
+});
 
 
 
