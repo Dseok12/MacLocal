3cf36f19c4bcef4ce899df7032573a6e4eecefd4
D.seok <ehskstkrjsa@naver.com>
2022-04-10 21:55:12 +0900

    React: Alone-Test SessionStrage Test

--- React_File/Study/Study01/alone-test01/src/TextComponent/TextComponent.js
+++ React_File/Study/Study01/alone-test01/src/TextComponent/TextComponent.js
@@ -3,22 +3,38 @@ import './TextComponent.css';
 
 const TextComponent = () => {
   const [test, setTest] = useState('');
-  const setStorage = () => {
-    window.localStorage.setItem('test', JSON.stringify(setTest('여기봐라~')))
+  const setStorage = (text01) => {
+    setTest(text01)
+    window.sessionStorage.setItem('test', JSON.stringify(text01))
+  }
+  const clearStorage = () => {
+    setTest('')
+    window.sessionStorage.removeItem('test')
   }
   return(
     <div>
       <div className="inner">
         <h1>Hi, TextComponent</h1>
+        <input
+          type='text'
+        />
         <p>{test}</p>
         <button
           type="button"
           onClick = {
             () => {
-              setStorage()
+              setStorage('여기봐라~')
             }
           }
         >click!!</button>
+        <button
+          type="button"
+          onClick = {() => {
+            clearStorage()
+          }}
+        >
+          clear!!
+        </button>
       </div>
     </div>
   )
