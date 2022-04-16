e5149d3bbb5e57d134a425c83fd09f66042a2896
D.seok <ehskstkrjsa@naver.com>
2022-04-16 16:55:44 +0900

    React: CodingApple - jlog

--- React_File/Study/Study02_CodingApple/jlog/src/App.js
+++ React_File/Study/Study02_CodingApple/jlog/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '중랑 막창 맛집']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 제목바꾸기 () {
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
+  }
+
+  let posts = '강남 고기 맛집';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <button onClick={ 제목바꾸기 }>버튼</button>
+      <div className="list">
+        <h3>{ 글제목[0] }<span onClick={ () => {따봉변경(따봉+1)} }>😍</span> {따봉}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
