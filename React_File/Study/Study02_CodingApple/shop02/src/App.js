e1a84242931903a8ff1a50c8d7ce645e43d12d18
D.seok <ehskstkrjsa@naver.com>
2022-04-18 14:50:21 +0900

    React: Coding-Apple - Shop02

--- React_File/Study/Study02_CodingApple/shop02/src/App.js
+++ React_File/Study/Study02_CodingApple/shop02/src/App.js
@@ -5,6 +5,8 @@ import {
 } from 'react-bootstrap';
 import Data from './data';
 import Card from "./Card";
+import Detail from "./Detail";
+import {Link, Route, Switch} from 'react-router-dom'
 import './App.css';
 
 function App() {
@@ -15,41 +17,53 @@ function App() {
     <div className="App">
       
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Shop</Navbar.Brand>
+        <Navbar.Brand><Link to='/'>Shop</Link></Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link><Link to='/'>Home</Link></Nav.Link>
+          <Nav.Link><Link to='/detail'>Detail</Link></Nav.Link>
+          <Nav.Link>Pricing</Nav.Link>
         </Nav>
       </Navbar>
 
-      <div class="jumbotron" className="background">
-        <h1 class="display-4">WellCome! Shop</h1>
-        <p class="lead">
-          This is a simple hero unit, a simple jumbotron-style
-          component for calling extra attention to featured
-          content or information.
-        </p>
-        <hr class="my-4" />
-        <p>
-          It uses utility classes for typography and spacing
-          to space content out within the larger container.
-        </p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </div>
+      
+      <Switch>
+        <Route exact path='/'>
+          <div class="jumbotron" className="background">
+            <h1 class="display-4">WellCome! Shop</h1>
+            <p class="lead">
+              This is a simple hero unit, a simple jumbotron-style
+              component for calling extra attention to featured
+              content or information.
+            </p>
+            <hr class="my-4" />
+            <p>
+              It uses utility classes for typography and spacing
+              to space content out within the larger container.
+            </p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </div>
 
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map((글, i) => {
-              return <Card
-                      shoes작명={shoes[i]}
-                      i작명={i}
-                    />
-            })
-          }
-        </div>
-      </div>
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map((글, i) => {
+                  return <Card
+                          shoes작명={shoes[i]}
+                          i작명={i}
+                        />
+                })
+              }
+            </div>
+          </div>
+        </Route>
+
+        <Route path='/detail'>
+          <Detail />
+        </Route>
+
+        <Route path='/:id'></Route>
+      </Switch>
+      
 
     </div>
   );
