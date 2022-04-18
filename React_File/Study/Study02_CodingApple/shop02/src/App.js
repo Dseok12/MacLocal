a2e4c754b948854a150669722114d7d6d0f3568a
D.seok <ehskstkrjsa@naver.com>
2022-04-18 14:09:28 +0900

    React: Coding-Apple - Shop02

--- React_File/Study/Study02_CodingApple/shop02/src/App.js
+++ React_File/Study/Study02_CodingApple/shop02/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React, { useState } from "react";
+import {
+  Navbar, Nav
+} from 'react-bootstrap';
+import Data from './data';
+import Card from "./Card";
 import './App.css';
 
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Shop</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
+
+      <div class="jumbotron" className="background">
+        <h1 class="display-4">WellCome! Shop</h1>
+        <p class="lead">
+          This is a simple hero unit, a simple jumbotron-style
+          component for calling extra attention to featured
+          content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <hr class="my-4" />
+        <p>
+          It uses utility classes for typography and spacing
+          to space content out within the larger container.
+        </p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {
+            shoes.map((글, i) => {
+              return <Card
+                      shoes작명={shoes[i]}
+                      i작명={i}
+                    />
+            })
+          }
+        </div>
+      </div>
+
     </div>
   );
 }
