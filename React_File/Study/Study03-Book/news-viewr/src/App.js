2338d109c6296c1595e620aa151873381abe4304
D.seok <ehskstkrjsa@naver.com>
2022-04-26 22:18:38 +0900

    React: Book - NEWS

--- React_File/Study/Study03-Book/news-viewr/src/App.js
+++ React_File/Study/Study03-Book/news-viewr/src/App.js
@@ -1,29 +1,9 @@
-import React, {useState} from "react";
-import axios from "axios";
+import React from "react";
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data,setData] = useState(null);
-  const onClick = async () => {
-    try{
-      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=fe3a727c3dd3403890cfad606e65b73d');
-      setData(response.data);
-    } catch (e) {
-      console.log(e)
-    };
-
-  }
-
   return (
-    <div>
-      <div>
-        <button
-          onClick={onClick}
-        >불러오기</button>
-      </div>
-      {
-        data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />
-      }
-    </div>
+    <NewsList />
   )
 }
 
--- React_File/Study/Study03-Book/news-viewr/src/components/NewsItem.js
+++ React_File/Study/Study03-Book/news-viewr/src/components/NewsItem.js
@@ -25,14 +25,38 @@ const NewsItemBlock = styled.div`
       margin-top: 0.5rem;
       white-space: normal;
     }
+    & + & {
+      margin-top: 3rem;
+    }
   }
 `;
 
 
 const NewsItem = ({ article }) => {
+
+  const { title, description, url, urlToImage } = article;
+
   return (
     <div>
-
+      <NewsItemBlock>
+        {
+          urlToImage && (
+            <div className="thumbnail">
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                <img src={urlToImage} alt="thumbnail" />
+              </a>
+            </div>
+          )
+        }
+        <div className="contents">
+          <h2>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              {title}
+            </a>
+          </h2>
+          <p>{description}</p>
+        </div>
+      </NewsItemBlock>
     </div>
   )
 }
