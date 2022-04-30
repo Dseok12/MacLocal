955336e10d259e65499d5ce36902113a6643eee4
D.seok <ehskstkrjsa@naver.com>
2022-04-30 19:31:48 +0900

    React: Book - NEWS

--- React_File/Study/Study03-Book/news-viewr/src/App.js
+++ React_File/Study/Study03-Book/news-viewr/src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import Categories from "./components/Categories";
 import NewsList from './components/NewsList';
 
 const App = () => {
+
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), [])
+
   return (
-    <NewsList />
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
   )
 }
 
--- React_File/Study/Study03-Book/news-viewr/src/components/NewsList.js
+++ React_File/Study/Study03-Book/news-viewr/src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import axios from "axios";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -15,7 +16,6 @@ const NewsListBlock = styled.div`
   }
 `;
 
-
 const sampleArticle = {
   title: '제목',
   description: '내용',
@@ -24,14 +24,42 @@ const sampleArticle = {
 }
 
 const NewsList = () => {
+
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try{
+        const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=fe3a727c3dd3403890cfad606e65b73d',);
+        setArticles(response.data.articles)
+      } catch (e) {
+        console.error(e)
+      }
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  // 대기 중일 때 
+  if(loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>
+  }
+
+  // 아직 articles 값이 설정되지 않았을 때
+  if(!articles){
+    return null;
+  }
+
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {
+        articles.map(article => (
+          <NewsItem key={article.url} article={article} />
+        ))
+      }
     </NewsListBlock>
   )
 }
