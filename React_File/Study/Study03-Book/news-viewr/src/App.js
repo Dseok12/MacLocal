445758b46e6b72e56ec8f6af67fe5451d9d800ee
D.seok <ehskstkrjsa@naver.com>
2022-06-14 22:44:01 +0900

    React

--- React_File/Study/Study03-Book/news-viewr/src/App.js
+++ React_File/Study/Study03-Book/news-viewr/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import NewsPage from "./pages/NewsPage";
 
 const App = () => {
 
   return (
     <>
-    <Routes>
       <Route path="/:category?" componnet={NewsPage} />
-    </Routes>
     </>
   )
 }
