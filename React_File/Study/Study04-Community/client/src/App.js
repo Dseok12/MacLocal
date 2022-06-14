30e199b24d111ffd32093c60a235aa59f80da206
D.seok <ehskstkrjsa@naver.com>
2022-06-14 23:27:27 +0900

    React

--- React_File/Study/Study04-Community/client/src/App.js
+++ React_File/Study/Study04-Community/client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Test from './Test';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>최상위 컴포넌트입니다.</h1>
+      <Test></Test>
     </div>
   );
 }
---
+++ React_File/Study/Study04-Community/client/src/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Test() {
+  return(
+    <div className="Test">
+      Test페이지 입니다.
+    </div>
+  )
+}
+
+export default Test;
