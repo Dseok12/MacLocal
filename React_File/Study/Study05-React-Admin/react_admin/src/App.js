ff6f637368be4ce87a3a699de60d58a21833b249
D.seok <ehskstkrjsa@naver.com>
2022-06-23 23:31:41 +0900

    React-admin Tutorial

--- React_File/Study/Study05-React-Admin/react_admin/src/App.js
+++ React_File/Study/Study05-React-Admin/react_admin/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+      </Admin>
     </div>
   );
 }
