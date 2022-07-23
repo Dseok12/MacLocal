7a148fa136fb296857967a028f95e59945ad29c0
D.seok <ehskstkrjsa@naver.com>
2022-07-23 23:14:12 +0900

    Hard Coding - 연습해야겠다..

--- Hard_Code/make_anything/make02/js/index.js
+++ Hard_Code/make_anything/make02/js/index.js
@@ -1,6 +1,28 @@
+const _get = (target) => {
+  return document.querySelector(target);
+}
 
 
+const main = () => {
 
+  const plusBtn = _get('.plus');
+  const minusBtn = _get('.minus');
+  const total = _get('.total');
+
+  plusBtn.addEventListener('click', function test01() {
+    console.log('hi')
+  })
+  minusBtn.addEventListener('click', function test02() {
+    console.log('bye')
+    total.nodeValue+1
+  })
+}
+
+
+const init = () => {
+  window.addEventListener('DOMContentLoaded', main)
+}
+init();
 
 
 
