c2fb3e807dd5a82360e26e5f85475a8b608570e8
D.seok <ehskstkrjsa@naver.com>
2022-07-31 18:17:37 +0900

    React-Client

--- React_File/Study/Study04-Community/client/src/index.js
+++ React_File/Study/Study04-Community/client/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter  } from 'react-router-dom';
+// import { createStore } from 'redux'; 
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// let store = createStore(() => { return [{ key: "value" }] });
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 reportWebVitals();
