af2fdfdf0adafc58114ca5dc602f04140db97a59
D.seok <ehskstkrjsa@naver.com>
2022-08-03 21:51:48 +0900

    React-Udong Sample

---
+++ React_File/Study/Study06/udong/src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'Header.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header;
