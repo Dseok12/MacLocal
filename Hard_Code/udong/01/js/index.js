4cf68f7961cff26a20bdebd57d86739c525429ab
D.seok <ehskstkrjsa@naver.com>
2022-08-10 22:07:51 +0900

    Udong

--- Hard_Code/udong/01/js/index.js
+++ Hard_Code/udong/01/js/index.js
@@ -2,23 +2,19 @@ const _get = (target) => {
   return document.querySelector(target)
 }
 
-const listItem = _get('.list_item')
+const listItem = _get('.list_item');
 const inputText = _get('.text_input');
 const inputTextValue = inputText.value;
 const Btn = _get('.btn');
 
-
 const btnCall = () => {
   Btn.addEventListener('click', (e) => {
+    const inputText = _get('.text_input');
     e.preventDefault();
-    // 여기에 localStroage로 보낼 기능 구현
-    if(inputTextValue == ''){
-      alert('리스트를 입력하세요!')
-    } else {
-      // 여기에 localStroage로 보낼 기능 구현
-      localStorage.getItem(inputTextValue)
-    }
-  })
+    listItem.innerText = inputText.value;
+    inputText.value = '';
+    inputText.focus();
+  });
 }
 
 const main = () => {
@@ -30,5 +26,3 @@ const init = () => {
 }
 
 document.addEventListener('DOMContentLoaded',init());
-
-
