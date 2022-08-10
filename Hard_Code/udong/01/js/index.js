811221b78a8d95be57520e8150a6922dd6e8ac92
D.seok <ehskstkrjsa@naver.com>
2022-08-10 23:22:10 +0900

    Udong

--- Hard_Code/udong/01/js/index.js
+++ Hard_Code/udong/01/js/index.js
@@ -2,6 +2,7 @@ const _get = (target) => {
   return document.querySelector(target)
 }
 
+
 const listItem = _get('.list_item');
 const inputText = _get('.text_input');
 const inputTextValue = inputText.value;
@@ -9,11 +10,20 @@ const Btn = _get('.btn');
 
 const btnCall = () => {
   Btn.addEventListener('click', (e) => {
-    const inputText = _get('.text_input');
     e.preventDefault();
-    listItem.innerText = inputText.value;
-    inputText.value = '';
-    inputText.focus();
+    const inputText = _get('.text_input');
+    const listUl = _get('.list_box');
+    // listUl.createElement('li');
+    listUl.innerHTML = `
+    <li class="list_item">${inputText.value}</li>
+    `
+    // listItem.innerText = inputText.value;
+    if(inputText.value == ''){
+      alert('항목을 입력하세요.');
+    } else {
+      inputText.value = '';
+      inputText.focus();
+    }
   });
 }
 
