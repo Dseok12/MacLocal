f776aa785306502092bb9a372e5376ef0ebdbcbe
D.seok <ehskstkrjsa@naver.com>
2022-09-13 23:17:01 +0900

    React : Retry

--- React_File/Study/Study08_Retry/client/src/App.js
+++ React_File/Study/Study08_Retry/client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Test3 from './Test3';
+import { Routes, Route } from 'react-router-dom';
+import Heading from './components/Heading';
+import List from './components/List';
+import Upload from './components/Upload';
 
 function App() {
   return (
     <div className="App">
-      <Test3 />
+      {/* <Test3 /> */}
+      <Heading />
+      <Routes>
+        <Route
+          path="/list"
+          element={<List />}
+        ></Route>
+        <Route
+          path="/upload"
+          element={<Upload />}
+        ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ React_File/Study/Study08_Retry/client/src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Heading() {
+  return (
+    <>
+      <h1>Hello, React</h1>
+    </>
+  );
+}
+
+export default Heading;
