d5a8b9a8c84a7815a1fe4cfe10c46b0118400231
D.seok <ehskstkrjsa@naver.com>
2022-10-03 19:20:45 +0900

    TabMenu

--- Hard_Code/Source_Collection/source03/js/index.js
+++ Hard_Code/Source_Collection/source03/js/index.js
@@ -1,6 +1,8 @@
-
-
-
-
-
-
+$(document).ready(function () {
+  $('.tab_box p').click(function () {
+    $(this).addClass('on');
+    $(this).siblings().removeClass('on');
+    $('.content_box p').removeClass('on');
+    $('#' + $(this).attr('data-alt')).addClass('on');
+  });
+});
