22dbaa97b31e08efc8cfec5814875e55e5275aa2
D.seok <ehskstkrjsa@naver.com>
2022-10-03 23:04:27 +0900

    React - My Homepoage

---
+++ React_File/Study/Study09_MyHome01/myhomepage/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './Reset.css';
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="Wrap">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ React_File/Study/Study09_MyHome01/myhomepage/src/components/Body/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Body.scss'
+
+function Body() {
+  return (
+    <div>Body</div>
+  )
+}
+
+export default Body
