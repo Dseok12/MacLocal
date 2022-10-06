dfec6ae6308323ff3a961f03f32c1a5f57a1fdcc
D.seok <ehskstkrjsa@naver.com>
2022-10-06 23:16:31 +0900

    React - My Home Page

--- React_File/Study/Study09_MyHome01/myhomepage/src/components/Footer/Footer.js
+++ React_File/Study/Study09_MyHome01/myhomepage/src/components/Footer/Footer.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import './Footer.scss';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div className='FooterWrap'>
-      <div className='inner'>Footer</div>
+      <div className='inner'>
+        <h2 className='footer_logo'>
+          <Link to='/'>Logo</Link>
+        </h2>
+        <div className='copy_rigth_box'>
+          <p className='copy_right'>CopyRightⒸ 2022 Make D.seok</p>
+        </div>
+        <div className='contact_wrap'>
+          <ul className='contact_box'>
+            <li className='address'>
+              서울시 중랑구
+            </li>
+            <li className='phone'>
+              <a href='tel:내 전화번호'>내 전화번호</a>
+            </li>
+            <li className='email'>
+              <a href="mailto:ehskstkrjs@naver.com">ehskstkrjs@naver.com</a>
+            </li>
+          </ul>
+        </div>
+        <div className='sns_wrap'>
+          <ul className='sns_box'>
+            <li>
+              <a
+                href="https://github.com/Dseok12"
+                target="_blank"
+                rel="noopener noreferrer"
+              >깃허브</a>
+            </li>
+            <li>
+              <a
+                href="https://blog.naver.com/ehskstkrjsa"
+                target="_blank"
+                rel="noopener noreferrer"
+              >블로그</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
