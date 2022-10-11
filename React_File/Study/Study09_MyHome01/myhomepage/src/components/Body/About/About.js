8801bf62c476dbf50daf2aec604b6f023f74bbb6
D.seok <ehskstkrjsa@naver.com>
2022-10-11 22:44:52 +0900

    React Axios Get

--- React_File/Study/Study09_MyHome01/myhomepage/src/components/Body/About/About.js
+++ React_File/Study/Study09_MyHome01/myhomepage/src/components/Body/About/About.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function About() {
+
+  const [getData, setGetData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.instantwebtools.net/v1/airlines')
+    .then((res) => {
+      console.log(res.data[0].name);
+      console.log('성공');
+      setGetData(res.data[0].name);
+      // for(let i = 0; i <= res.data.length; i++){
+      //   setGetData(res.data[i].name);
+      // }
+    }).catch((err) => {
+      console.error(err);
+      console.log('실패')
+    });
+  }, []);
+
   return (
     <div className='AboutWrap'>
-      <div className='inner'>About</div>
+      <div className='inner'>
+        <p>{getData}</p>
+        <p>About</p>
+      </div>
     </div>
   )
 }
 
-export default About
+export default About;
