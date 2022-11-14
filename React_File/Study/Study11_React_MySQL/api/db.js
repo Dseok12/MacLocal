7cc2237ec6e6add0b10b0d351cd2fd2d517f3b76
D.seok <ehskstkrjsa@naver.com>
2022-11-14 22:58:49 +0900

    React MySQL API SERVER

---
+++ React_File/Study/Study11_React_MySQL/api/db.js
@@ -0,0 +1,8 @@
+import mysql from 'mysql';
+
+export const db = mysql.createConnection({
+  host: "localhost",
+  user: process.env.APP_USER_ID,
+  password: process.env.APP_USER_PASSWORD,
+  database: process.env.APP_USER_DATABASE
+})
