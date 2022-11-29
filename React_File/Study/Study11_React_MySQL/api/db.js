8bdd421c6d38ed2ece33a49519114212f7c7ea19
D.seok <ehskstkrjsa@naver.com>
2022-11-29 23:25:11 +0900

    React Server

--- React_File/Study/Study11_React_MySQL/api/db.js
+++ React_File/Study/Study11_React_MySQL/api/db.js
@@ -3,9 +3,8 @@ import mysql from 'mysql';
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "dnjstjr12@",
-  database: "blog"
+  database: "blog",
   // user: process.env.APP_USER_ID,
-  // password: process.env.APP_USER_PASSWORD,
+  password: process.env.APP_USER_PASSWORD,
   // database: process.env.APP_USER_DATABASE
 });
