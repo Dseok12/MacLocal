a2a881c72e57ac2be1355d97dd004013122721dd
D.seok <ehskstkrjsa@naver.com>
2022-12-17 17:48:04 +0900

    Com

---
+++ Hard_Code/company/01/js/index.js
@@ -0,0 +1,26 @@
+// setInterval(function(){
+//   $('.obj').children('img').toggleClass('on')
+// }, 1000)
+
+function updateDivs() {
+  const parentDivs = document.querySelectorAll('.obj');
+
+  parentDivs.forEach(parentDiv => {
+      const onDivs = parentDiv.querySelectorAll('.on');
+      onDivs.forEach(div => div.classList.remove('on'));
+
+      const childDivs = parentDiv.querySelectorAll('.obj img');
+      const randomIndex = Math.floor(Math.random() * childDivs.length);
+      const randomDiv = childDivs[randomIndex];
+
+      randomDiv.classList.add('on');
+  });
+}
+updateDivs();
+setInterval(updateDivs, 1000);
+
+
+
+
+
+
