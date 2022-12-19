950a6ef356466e37f5cc9be509b42a465d0ffecc
D.seok <ehskstkrjsa@naver.com>
2022-12-19 23:05:43 +0900

    React

--- React_File/Study/Study13_React_MySQL/client/src/App.js
+++ React_File/Study/Study13_React_MySQL/client/src/App.js
@@ -1,9 +1,15 @@
 import "./_Reset.css";
+import "./_test.css";
 
 function App() {
   return (
     <div className="App">
       Hello, React!
+
+      <div className="s_links">
+        <a href="https://www.youtube.com/watch?v=0aPLk2e2Z3g&t=1089s">링크</a>
+        <p>4:08</p>
+      </div>
     </div>
   );
 }
