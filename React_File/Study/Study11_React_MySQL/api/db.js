fda688445858687013c5f40ecb6569abe2cb9ae9
D.seok <ehskstkrjsa@naver.com>
2023-01-01 23:29:25 +0900

    Push

--- React_File/Study/Study11_React_MySQL/api/db.js
+++ React_File/Study/Study11_React_MySQL/api/db.js
@@ -3,8 +3,9 @@ import mysql from 'mysql';
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
+  password: "dnjstjr12@",
   database: "blog",
   // user: process.env.APP_USER_ID,
-  password: process.env.APP_USER_PASSWORD,
+  // password: process.env.APP_USER_PASSWORD,
   // database: process.env.APP_USER_DATABASE
 });
