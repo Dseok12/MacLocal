131102b139aba74a1fd26fabe50b14b39f5cae31
D.seok <ehskstkrjsa@naver.com>
2023-01-08 23:13:47 +0900

    11. Register New User

--- React_File/Study/Study14_MERN01/backend/controllers/userController.js
+++ React_File/Study/Study14_MERN01/backend/controllers/userController.js
@@ -1,10 +1,47 @@
-const registerUser = (req, res) => {
-  if(!req.body.email){
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+
+const registerUser = asyncHandler(async (req, res) => {
+  const {name, email, password} = req.body
+
+  // Validation
+  if(!name || !email || !password){
     res.status(400)
-    throw new Error("이메일을 추가해주세요.")
+    throw new Error("닉네임, 이메일, 비밀번호를 모두 작성해주세요.");
   }
-  res.send("Register User")
-};
+
+  if(password.length < 6){
+    res.status(400)
+    throw new Error("비밀번호는 6자리 이상 사용해주세요.");
+  }
+
+  // 사용자가 이메일이 이미 존재할 때.
+  const userExists = await User.findOne({email});
+
+  if(userExists) {
+    res.status(400)
+    throw new Error("이미 존재하는 이메일입니다.");
+  }
+
+  // 새로운 사용자 등록하기
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  })
+
+  if(user) {
+    const {_id, name, email, photo, phone, bio} = user
+    res.status(201).json({
+      _id, name, email, photo, phone, bio
+    })
+  } else {
+    res.status(400)
+    throw new Error("정보가 일치 하지 않습니다.");
+  }
+
+});
 
 
 module.exports = {
