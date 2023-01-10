3fbd4a3194ec1f30e71a2bbe3236c475c4817283
D.seok <ehskstkrjsa@naver.com>
2023-01-10 22:11:51 +0900

    Generate JSON Web Token

--- React_File/Study/Study14_MERN01/backend/controllers/userController.js
+++ React_File/Study/Study14_MERN01/backend/controllers/userController.js
@@ -1,7 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
-
+// 데이터베이스에 토큰 넣기
+const generateToken = (id) => {
+  jwt.sign({id}, process.env.JWT_SECRET, {expiresIn:"1d"})
+}
 
 const registerUser = asyncHandler(async (req, res) => {
   const {name, email, password} = req.body
@@ -30,12 +34,15 @@ const registerUser = asyncHandler(async (req, res) => {
     name: name,
     email: email,
     password: password,
-  })
+  });
+
+  // Generate Token 데이터베이스에 토큰 넣기
+  const token = generateToken(user._id)
 
   if(user) {
     const {_id, name, email, photo, phone, bio} = user
     res.status(201).json({
-      _id, name, email, photo, phone, bio
+      _id, name, email, photo, phone, bio, token
     })
   } else {
     res.status(400)
