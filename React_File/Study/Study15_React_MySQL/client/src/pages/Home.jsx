f4915bcaf0bbd4d9bd15b884dc513dfaa8a0990d
D.seok <ehskstkrjsa@naver.com>
2023-01-16 23:00:29 +0900

    서버 움직임

--- React_File/Study/Study15_React_MySQL/client/src/pages/Home.jsx
+++ React_File/Study/Study15_React_MySQL/client/src/pages/Home.jsx
@@ -52,7 +52,7 @@ const Home = () => {
         <p>
           <a href="https://www.youtube.com/watch?v=0aPLk2e2Z3g&t=1089s">링크</a>
         </p>
-        연습: 53:57
+        연습: 1:04:41
       </div>
     </div>
   )
