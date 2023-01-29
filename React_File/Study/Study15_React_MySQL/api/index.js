bad744c7be25ab8df0290636f3784e6bc45f8d96
D.seok <ehskstkrjsa@naver.com>
2023-01-29 15:56:32 +0900

    리액트, MySQL, 노드 서버에 이미지 업로드 시키기. feat.multer

--- React_File/Study/Study15_React_MySQL/api/index.js
+++ React_File/Study/Study15_React_MySQL/api/index.js
@@ -3,11 +3,33 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
 import cookieParser from "cookie-parser";
+import multer from "multer"; // multer -> 이미지 파일 업로드 시켜주는 라이브러리 { https://www.npmjs.com/package/multer }
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
+// 여기서 오류가 났었음. 작업 내용 -> 이미지 업로드하기 오류 / multer 사용법
+// 여기서 멈추면 이미지가 컴파일이 안되서 DiskStorage 설정을 따로 해줘야함.
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
--- React_File/Study/Study15_React_MySQL/client/src/pages/Write.jsx
+++ React_File/Study/Study15_React_MySQL/client/src/pages/Write.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -5,13 +6,37 @@ import 'react-quill/dist/quill.snow.css';
 const Write = () => {
 
   const [value, setValue] = useState('');
+  const [title, setTitle] = useState('');
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState('');
+
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await axios.post("/upload", formData);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const imgUrl = upload()
+    try{
+
+    }catch(err){
+      console.log(err);
+    }
+  }
 
   // console.log(value)
 
   return (
     <div className='add'>
       <div className='content'>
-        <input type="text" placeholder='Title' />
+        <input type="text" placeholder='제목을 입력하세요.' onChange={ e => setTitle(e.target.value) } />
         <div className="editorContainer">
           <ReactQuill theme="snow" value={value} onChange={setValue} />
         </div>
@@ -25,29 +50,41 @@ const Write = () => {
           <span>
             <b>Visibility: </b> Public
           </span>
-          <input style={{display: "none"}} type="file" id='file' name='' />
-          <label htmlFor='file'>이미지 업로드</label>
+          <input
+            style={{ display: "none" }}
+            type="file"
+            id="file"
+            name=""
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <label className="file" htmlFor="file">
+            이미지 업로드
+          </label>
+          <div className="buttons">
+            <button>저장하기</button>
+            <button onClick={handleClick}>등록하기</button>
+          </div>
         </div>
         <div className='item'>
           <h1>카테고리</h1>
           <div className="cat">
-            <input type="radio" name='cat' value="art" id='art' />
+            <input type="radio" name='cat' value="art" id='art' onChange={ e => setCat(e.target.value) }  />
             <label htmlFor='art'>Art</label>
           </div>
           <div className="cat">
-            <input type="radio" name='cat' value="science" id='Science' />
+            <input type="radio" name='cat' value="science" id='Science' onChange={ e => setCat(e.target.value) }  />
             <label htmlFor='Science'>Science</label>
           </div>
           <div className="cat">
-            <input type="radio" name='cat' value="technology" id='technology' />
+            <input type="radio" name='cat' value="technology" id='technology' onChange={ e => setCat(e.target.value) }  />
             <label htmlFor='technology'>Technology</label>
           </div>
           <div className="cat">
-            <input type="radio" name='cat' value="cinema" id='cinema' />
+            <input type="radio" name='cat' value="cinema" id='cinema' onChange={ e => setCat(e.target.value) }  />
             <label htmlFor='cinema'>Cinema</label>
           </div>
           <div className="cat">
-            <input type="radio" name='cat' value="design" id='design' />
+            <input type="radio" name='cat' value="design" id='design' onChange={ e => setCat(e.target.value) }  />
             <label htmlFor='design'>Design</label>
           </div>
           <div className="cat">
