2f26fe3bd87b291a2225f476c7162d8c549210b4
D.seok <ehskstkrjsa@naver.com>
2023-01-30 22:02:34 +0900

    SNS 만들어보기

---
+++ React_File/Study/Study16_React_MySQL02/client/src/StudyLink.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './_StudyLink.css';
+
+const StudyLink = () => {
+  return (
+    <div className=''>
+      <ul className="tuto_box">
+          <li className="tuto_item">
+            <h2>MySQL 01</h2>
+            <p className="tuto_link">
+              <a href="https://www.youtube.com/watch?v=0aPLk2e2Z3g&t=1089s">https://www.youtube.com/watch?v=0aPLk2e2Z3g&t=1089s</a>
+            </p>
+            <p className="tuto_time">
+              연습: 2:19:30
+            </p>
+          </li>
+          <li className="tuto_item">
+            <h2>다음 튜토리얼</h2>
+            <p className="tuto_link">
+              <a href="https://www.youtube.com/watch?v=1EuNnZEp2sQ&t=204s">https://www.youtube.com/watch?v=1EuNnZEp2sQ&t=204s</a>
+            </p>
+            <p className="tuto_time">
+              연습: 00:07:19
+            </p>
+          </li>
+        </ul>
+    </div>
+  )
+}
+
+export default StudyLink
