3c80a9c9b972b8d6d8af1aef532464fe73dc77ea
D.seok <ehskstkrjsa@naver.com>
2023-02-09 23:56:31 +0900

    4. 클라이언트 깃 클론

--- React_File/Study/Study16_React_MySQL02/client/src/index.js
+++ React_File/Study/Study16_React_MySQL02/client/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthContextProvider } from "./context/authContext";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
+  <React.StrictMode>
+    <DarkModeContextProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </DarkModeContextProvider>
+  </React.StrictMode>
 );
---
+++ React_File/Study/Study16_React_MySQL02/client/src/pages/register/Register.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>원석's Social.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+          <button>로그인</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>회원가입</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>회원가입</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
