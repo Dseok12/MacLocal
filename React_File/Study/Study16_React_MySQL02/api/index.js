f51bbf370adb2f2a8dd37ce5221ffd2a692e226f
D.seok <ehskstkrjsa@naver.com>
2023-02-15 21:54:43 +0900

    6. 로그인 기능 만들기

--- React_File/Study/Study16_React_MySQL02/api/index.js
+++ React_File/Study/Study16_React_MySQL02/api/index.js
@@ -10,8 +10,15 @@ import cookieParser from "cookie-parser";
 
 
 /** Middlewares */
+app.use((req,res,next) => {
+  res.header("Access-Control-Allow-Credentials", true)
+  next()
+})
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000",
+  
+}))
 app.use(cookieParser())
 
 
