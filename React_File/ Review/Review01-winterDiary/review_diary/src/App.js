d1bc0c5ea108a3fe6683a0584f56383e1806833e
D.seok <ehskstkrjsa@naver.com>
2023-08-27 17:33:21 +0900

    winter 복습

---
+++ React_File/ Review/Review01-winterDiary/review_diary/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DiaryEidtor from './DiaryEidtor';
+
+function App() {
+  return (
+    <div className="App">
+      <DiaryEidtor />
+    </div>
+  );
+}
+
+export default App;
---
+++ React_File/ Review/Review01-winterDiary/review_diary/src/DiaryEidtor.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import "./DiaryEidtor.css";
+
+function DiaryEidtor() {
+
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    alert("성공!");
+  };
+
+  return (
+    <div className='DiaryEidtor'>
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <select
+          name="emotion"
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button
+          onClick={handleSubmit}
+        >완료</button>
+      </div>
+    </div>
+  )
+}
+
+export default DiaryEidtor;
