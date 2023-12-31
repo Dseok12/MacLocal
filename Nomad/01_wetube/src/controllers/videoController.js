3029a618dc76e6b38fd0210139f9970b371660a9
D.seok <ehskstkrjsa@naver.com>
2023-12-31 15:36:03 +0900

    회사지원

--- Nomad/01_wetube/src/controllers/videoController.js
+++ Nomad/01_wetube/src/controllers/videoController.js
@@ -1,36 +1,38 @@
 
+let videos = [
+  {
+    title: "Frist Video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+]
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Frist Video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ]
   return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const see = (req, res) => {
-  res.render("watch", { pageTitle: "Watch" });
+  const { id } = req.params;
+  const video = videos[id-1];
+  res.render("watch", { pageTitle: "Watch", pageTitle: `Watching ${video.title}`});
 };
 
 export const edit = (req, res) => {
