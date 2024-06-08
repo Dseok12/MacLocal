d2293bd04d3e1dddb3e4dcbea4c8410f99fcfba3
D.seok <ehskstkrjsa@naver.com>
2024-06-08 17:36:38 +0900

    Mac Setting

---
+++ Nomad/02_wetube/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+});
