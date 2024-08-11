fb180613a5eb4edaffed430d861c1eaa6abae1a7
D.seok <ehskstkrjsa@naver.com>
2024-08-11 14:00:54 +0900

    #3.3부터 다시듣기

--- Nomad/03_wetube/src/server.js
+++ Nomad/03_wetube/src/server.js
@@ -4,6 +4,12 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => {
+  console.log("Home")
+}
+
+app.get("/",handleHome)
+
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀🚀🚀`);
 
