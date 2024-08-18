d023cabd840a5860f5cdfc1edbb7388e6f8e7b6f
D.seok <ehskstkrjsa@naver.com>
2024-08-18 15:05:58 +0900

    Middleware 설명

--- Nomad/03_wetube/src/server.js
+++ Nomad/03_wetube/src/server.js
@@ -4,18 +4,32 @@ const PORT = 4400;
 
 const app = express();
 
-const gossipMiddleware = (res, req, next) => {
-  console.log("중간!");
-  // return res.send("allal")
+const logger = (req, res, next) => {
+  console.log(`${req.method } ${req.url}`);
+  next();
+}
+
+const privateMiddlewares = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  };
+  console.log("Allowed, you may continue.")
   next();
 }
 
 const handleHome = (req, res) => {
-  console.log("first")
-  return res.end();
+  return res.send("I love middlewares!");
+};
+
+const handleProtected = (req, res ) => {
+  return res.send("Welcome to the private lounge.")
 }
 
-app.get("/", gossipMiddleware, handleHome)
+app.use(logger);
+app.use(privateMiddlewares);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀🚀🚀`);
