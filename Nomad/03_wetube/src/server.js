5b8a64c00f38677925488577a790d653d6910651
D.seok <ehskstkrjsa@naver.com>
2024-08-18 16:17:46 +0900

    #4.1부터 듣기

--- Nomad/03_wetube/src/server.js
+++ Nomad/03_wetube/src/server.js
@@ -1,37 +1,20 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4400;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method } ${req.url}`);
-  next();
-}
-
-const privateMiddlewares = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  };
-  console.log("Allowed, you may continue.")
-  next();
-}
+const logger = morgan("dev")
 
 const handleHome = (req, res) => {
   return res.send("I love middlewares!");
 };
 
-const handleProtected = (req, res ) => {
-  return res.send("Welcome to the private lounge.")
-}
-
-app.use(logger);
-app.use(privateMiddlewares);
+app.use(logger)
 app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
-const handleListening = () =>
+const handleListening = () =>{
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀🚀🚀`);
+};
 
 app.listen(PORT, handleListening);
