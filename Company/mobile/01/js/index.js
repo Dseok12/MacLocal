543d30fc9f6839ce46a76be84452a3a78505cec8
D.seok <ehskstkrjsa@naver.com>
2024-08-18 18:00:37 +0900

    tab Common

--- Company/mobile/01/js/index.js
+++ Company/mobile/01/js/index.js
@@ -1,4 +1,8 @@
-
+$(".tabBox li a").on("click", function() {
+  let index = $(".tabBox li a").index(this)
+  $(".tab__contents--box").removeClass('on');
+  $('.tab__contents--box:eq('+ index +')').addClass('on');
+})
 
 
 
