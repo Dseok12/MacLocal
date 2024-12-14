1cf8e88dbf7f9deec0363f0d05ab327977128ef4
D.seok <ehskstkrjsa@naver.com>
2024-12-14 23:53:51 +0900

    Link

--- React_File/winter/20241214/dairy/src/App.jsx
+++ React_File/winter/20241214/dairy/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './_Reset.css';
 import './App.css';
 import Dairy from './page/Dairy';
@@ -8,24 +8,31 @@ import Notefound from './page/Notefound';
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-      <Route
-        path="/new"
-        element={<New />}
-      />
-      <Route
-        path="/dairy"
-        element={<Dairy />}
-      />
-      <Route
-        path="*"
-        element={<Notefound />}
-      />
-    </Routes>
+    <>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/new'}>New</Link>
+        <Link to={'/diary'}>Diary</Link>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/new"
+          element={<New />}
+        />
+        <Route
+          path="/dairy"
+          element={<Dairy />}
+        />
+        <Route
+          path="*"
+          element={<Notefound />}
+        />
+      </Routes>
+    </>
   );
 }
 
